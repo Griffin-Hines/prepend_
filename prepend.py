aa32eb1302fab630cb4c5218341efa7d237b24ef
Griffin-Hines <143197327+Griffin-Hines@users.noreply.github.com>
2023-10-30 13:05:31 -0400

    Create prepend.py

---
+++ prepend.py
@@ -0,0 +1,13 @@
+import os
+
+directory = "./"
+
+file_list = os.listdir(directory)
+
+for filename in file_list:
+        full_path = os.path.join(directory, filename)
+        new_name = '_' + filename
+        if(os.path.isfile(full_path)):
+                os.rename(full_path, os.path.join(directory, new_name))
+        elif(os.path.isdir(full_path)):
+                os.rename(full_path, os.path.join(directory, new_name))
